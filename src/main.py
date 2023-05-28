8b05f2aa6afb35e91538d28d734d329d49429f04
Jeffro Tanvik <00002@protonmail.com>
2023-05-28 13:22:07 +0800

    update cmc news

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from push import cubox
 
 from spyders.block_beats import block_beats
+import spyders
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     all_articles = []
     all_articles.extend(block_beats.get_articles())
+    all_articles.extend(spyders.cmc.news.get_articles())
 
     cubox.push_to_cubox(all_articles=all_articles)
---
+++ src/spyders/cmc/news.py
@@ -0,0 +1,37 @@
+import bs4.element
+import requests
+from bs4 import BeautifulSoup
+
+TAG = "CMC News"
+BASE_URL = "https://coinmarketcap.com"
+
+
+def get_articles():
+    all_articles = []
+    html = requests.get('https://coinmarketcap.com/headlines/news/').text
+    bs = BeautifulSoup(html, 'html.parser')
+
+    articles = bs.find_all(name='div', attrs={"class": "coCmGz"})
+    for article in articles:
+        title = article.find(name='a').text
+        url = article.find(name='a').get('href')
+        description = None
+
+        # 补全url
+        if url.startswith('/'):
+            url = BASE_URL + url
+
+        if title is not None and url is not None:
+            all_articles.append(
+                {
+                    "title": title,
+                    "url": BASE_URL + url,
+                    "description": description,
+                    "tags": [TAG, "Blockchain"]
+                }
+            )
+    return all_articles
+
+
+if __name__ == '__main__':
+    print(get_articles())
