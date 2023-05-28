bd50a50a8921aee01fea35e25d1e90ed5e24c0c0
Jeffro Tanvik <00002@protonmail.com>
2023-05-28 13:25:12 +0800

    bug fix

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from push import cubox
 
 from spyders.block_beats import block_beats
-import spyders
+from spyders.cmc import news as cmc_news
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     all_articles = []
     all_articles.extend(block_beats.get_articles())
-    all_articles.extend(spyders.cmc.news.get_articles())
+    all_articles.extend(cmc_news.get_articles())
 
     cubox.push_to_cubox(all_articles=all_articles)
