90d600f827b77e5c20a2cc312b989bc46ea96d10
Jeffro Tanvik <00002@protonmail.com>
2023-05-28 19:16:08 +0800

    update

--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ from spyders.block_beats import block_beats
 from spyders.cmc import news as cmc_news
 from spyders.followin import news as followin_news
 
+from spyders.newsbtc import rss as newsbtc_rss
+from spyders.pannews import rss as pannews_rss
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -11,5 +14,13 @@ if __name__ == '__main__':
     all_articles.extend(block_beats.get_articles())
     all_articles.extend(cmc_news.get_articles())
     # all_articles.extend(followin_news.get_articles())
+    all_articles.extend(newsbtc_rss.get_articles())
+    all_articles.extend(pannews_rss.get_articles())
+
+    # shuffle the articles
+    import random
+    random.shuffle(all_articles)
+
+    # print(len(all_articles))
 
     cubox.push_to_cubox(all_articles=all_articles)
