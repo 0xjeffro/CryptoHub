3348fd210200d6ab2f568e57f05a1904046c065d
Jeffro Tanvik <00002@protonmail.com>
2023-05-28 13:58:17 +0800

    update followin news

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from push import cubox
 
 from spyders.block_beats import block_beats
 from spyders.cmc import news as cmc_news
+from spyders.followin import news as followin_news
 
 
 # Press the green button in the gutter to run the script.
@@ -9,5 +10,6 @@ if __name__ == '__main__':
     all_articles = []
     all_articles.extend(block_beats.get_articles())
     all_articles.extend(cmc_news.get_articles())
+    all_articles.extend(followin_news.get_articles())
 
     cubox.push_to_cubox(all_articles=all_articles)
---
+++ src/spyders/followin/news.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+TAG = "Followin News"
+BASE_URL = "https://followin.io/zh-Hans/news"
+
+
+def get_articles():
+    all_articles = []
+    html = requests.get('https://followin.io/zh-Hans/news').text
+    bs = BeautifulSoup(html, 'html.parser')
+
+    articles = bs.find_all(name='a', attrs={"target": "_self"})
+    for article in articles:
+        title = article.text
+        url = article.get('href')
+        description = None
+        # 补全url
+        if url.startswith('/'):
+            url = BASE_URL + url
+
+        if title is not None and url is not None:
+            all_articles.append(
+                {
+                    "title": title,
+                    "url": url,
+                    "description": description,
+                    "tags": [TAG, "Blockchain"]
+                }
+            )
+    return all_articles
+
+
+if __name__ == '__main__':
+    print(get_articles())
